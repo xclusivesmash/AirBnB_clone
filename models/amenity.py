5c70e922d564e01bc615be9b21233b1645b4a574
xclusivesmash <sbumatshiane916@gmail.com>
2024-01-09 10:14:46 +0200

    Added amenity class file

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module: amenity
+Description: Inherits from base model.
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Some description."""
+    name = ""
