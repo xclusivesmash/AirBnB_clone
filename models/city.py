fd7dfd1bd0db427c1477d7f1e033042d94a62462
xclusivesmash <sbumatshiane916@gmail.com>
2024-01-09 10:15:16 +0200

    Added city class file

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module: city
+Description: Inherits from base model
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Some description."""
+    state_id = ""
+    name = ""
