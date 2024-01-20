86e0f2ed67d553fd3b3e1e3fb6284a5b319e9eb5
xclusivesmash <sbumatshiane916@gmail.com>
2024-01-20 13:55:18 +0200

    Initial commit

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -3,3 +3,42 @@
 Module: test_city
 Description: Performs unit testing on city class.
 """
+import unittest
+import os
+from models.city import City
+
+
+class TestCity(unittest.TestCase):
+    """Test the City class."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up resources to be used."""
+        cls.cty = City()
+        cls.cty.id = "aksjhfcckzdvxguayg"
+        cls.cty.name = "Berlin"
+        return None
+
+    def test_isObjectInstance(self):
+        """Checks if self.cty is instance of City."""
+        self.assertIsInstance(self.cty, City)
+        return None
+
+    def test_id(self):
+        """Checks if id is a str."""
+        self.assertEqual(type(self.cty.id), str)
+        return None
+
+    @classmethod
+    def tearDownClass(cls):
+        """Remove all used-up resources."""
+        del cls.cty
+        try:
+            os.remove("file.json")
+        except FileNotFoundError:
+            pass
+        return None
+
+
+if __name__ == "__main__":
+    unittest.main()
