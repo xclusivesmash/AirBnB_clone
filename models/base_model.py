b15bf07bcec5725852ea9c39f8b9359c25e5d024
xclusivesmash <sbumatshiane916@gmail.com>
2024-02-24 13:28:34 +0200

    Initial commit

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,6 @@ import uuid
 from datetime import datetime
 import models
 
-
 _format = "%Y-%m-%dT%H:%M:%S.%f"
 
 
@@ -27,9 +26,6 @@ class BaseModel:
             save(self) -> updated the class instance after each modification.
             to_dict(self) -> gives a dict. rep. of an object.
        """
-    id = ""
-    created_at = None
-    updated_at = None
 
     def __init__(self, *args: list, **kwargs: dict) -> None:
         """Initialization method.
@@ -43,11 +39,11 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
-                    # do nothing, just pass to next.
+                    # do nothing, just pass to next item.
                     continue
                 setattr(self, key, value)
                 if type(self.updated_at) is str:
---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module: state
+Description: Inherits from base model.
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Some description."""
+    name = ""
